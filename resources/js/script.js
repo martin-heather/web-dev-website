818cf0b102da2b8c6b48f3c4fe3fced8902578a8
martin-heather <troglo.dight@gmail.com>
2019-09-14 15:53:03 -0400

    first commit

---
+++ resources/js/script.js
@@ -0,0 +1,51 @@
+function resize() {
+  let toc = document.getElementById('toc');
+  if (window.innerWidth > 768) {
+    toc.style.display = 'block';
+  } else {
+    toc.style.display = 'none';
+  }
+}
+
+window.onresize = resize;
+
+function toggleMenu() {
+  let toc = document.getElementById('toc');
+  if (toc.style.display === 'none') {
+    toc.style.display = 'block';
+  } else if (window.innerWidth > 768) {
+    toc.style.display = 'block';
+  } else {
+    toc.style.display = 'none';
+  }
+}
+
+function freelance() {
+  document.getElementById('freelance').innerHTML =
+    'I’ve copyedited hundreds of books as well as countless ads and marketing documents. I\'ve also proofread books and scholarly articles and provided other editorial services like fact-checking and multimedia quality control. I meet deadlines and client expectations without fail.<center><p><img src="./resources/img/modus.png" alt="Groupe Modus logo"><img src="./resources/img/harpercollins.png" alt="HarperCollins logo"><img src="./resources/img/harlequin.jpg" alt="Harlequin logo"><img src="./resources/img/nelson.png" alt="Nelson logo"></p></center>';
+}
+
+function inHouse() {
+  document.getElementById('inHouse').innerHTML =
+    'I\'ve worked in-house at two of Canada’s largest publishers. At Nelson Education, I helped create a wide variety of print and digital educational resources, and at Harlequin Books, I proofread and copyedited trade fiction and nonfiction books.<center><p></p><img src="./resources/img/nelson.png" alt="Nelson logo"><img src="./resources/img/harlequin.jpg" alt="Harlequin logo"></p></center>';
+}
+
+function concordia() {
+  document.getElementById('concordia').innerHTML =
+    '<img src="./resources/img/concordia.png" alt="Concordia University logo"><p><span>Concordia University</span></p><p>Since 2016, I have been studying French grammar, literature and composition part-time in the French Studies department and have a cumulative GPA of 4.0.</p>';
+}
+
+function ryerson() {
+  document.getElementById('ryerson').innerHTML =
+    '<img src="./resources/img/ryerson.png" alt="Ryerson University logo"><p><span>Ryerson University</span></p>At Ryerson, I studied editing, digital publishing and book design. I was twice nominated for the Rosemary Shipton Award for Excellence in Book Editing.';
+}
+
+function uvic() {
+  document.getElementById('uvic').innerHTML =
+    '<img src="./resources/img/uvic.png" alt="University of Victoria log"><p><span>University of Victoria</span></p>While attending UVic, I also worked for International Student Services, where I taught ESL students in the university\'s Faculty of Business.';
+}
+
+function guelph() {
+  document.getElementById('guelph').innerHTML =
+    '<img src="./resources/img/guelph.png" alt="University of Guelph logo"><p><span>University of Guelph</span></p>At UofG, I taught English usage and style at the university\'s Learning and Writing Centre as a student writing consultant.';
+}
